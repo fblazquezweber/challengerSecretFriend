d38187ceca38754e7da5d6c3e463c840973cf62c
Francisco Blazquez Weber <fblazquezweber@gmail.com>
2025-02-16 17:46:52 -0300

    Implementa función para agregar amigos con validación y actualización del array

--- app.js
+++ app.js
@@ -1,2 +1,33 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
+console.log("app.js cargado correctamente");
+
+function agregarAmigo(){
+    let amigo = document.getElementById('amigo').value.trim().toLowerCase();
+    //trim() se utiliza para eliminar espacios en blanco al inicio y al final de una cadena
+    //test() se utiliza para verificar si una cadena de texto cumple con una expresión regular 
+    if (/^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$/.test(amigo)){
+        if(!amigos.includes(amigo)){
+            amigos.push(amigo);
+        }else{
+            alert("Ese nombre ya está en la lista.");
+        }
+    } else{
+        if(amigo.length===0){
+            alert("Por favor, inserte un nombre.")
+        }else{
+            alert("El nombre contiene caracteres no válidos.")
+        }
+    }
+    limpiarEntrada();
+    return;
+}
+
+
+function limpiarEntrada(){
+    document.getElementById('amigo').value ="";
+}
+
+
+
+
